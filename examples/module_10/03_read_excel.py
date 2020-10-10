1303f26db4487b8ff8d8945adcef5cf242c96ccd
Luis Rivas <luis.m.rivas@oracle.com>
2020-10-10 13:41:41 -0500

    Add latest changes

--- examples/module_10/03_read_excel.py
+++ examples/module_10/03_read_excel.py
@@ -6,11 +6,14 @@ script_dir = os.path.dirname(__file__)
 script_dir = os.path.abspath(script_dir)
 abs_file_path = os.path.join(script_dir, 'example.xlsx')
 
-workbook = openpyxl.load_workbook(abs_file_path)
-worksheet = workbook['example']
 
-for row in worksheet.iter_rows(min_row=2):
-    cell_values = []
-    for cell in row:
-        cell_values.append(str(cell.value))
-    print(', '.join(cell_values))
+try:
+    workbook = openpyxl.load_workbook(abs_file_path)
+    worksheet = workbook['example2']
+    for row in worksheet.iter_rows(min_row=2):
+        cell_values = []
+        for cell in row:
+            cell_values.append(str(cell.value))
+        print(', '.join(cell_values))
+except:
+    print('Verifica si el archivo existe, y si la hoja es valida')
