ab0798c1f8147a21f67a9f0ca5edfcef616f0b70
Luis Rivas <luis.m.rivas@oracle.com>
2020-10-12 21:09:46 -0500

    Solution5 complete solution

--- solutions/module_11/05_solution.py
+++ solutions/module_11/05_solution.py
@@ -1,14 +1,33 @@
 """
 Abre la pagina: https://www.google.com/
 Verifica que la URL sea la correct
-Encuentra el elemento con el siguiente id:
+Encuentra el elemento con el siguiente:
 NAME = q
 Escribe cualquier búsqueda
 Cierra el navegador
 """
 from solutions.module_11.webdriver_factory import create_driver_instance
 
-driver = create_driver_instance('safari')
+driver = create_driver_instance('chrome')
+driver.implicitly_wait(15)
+driver.get('https://www.google.com/')
 
+# Test Case 1
+if driver.title == 'Google':
+    print('[PASS] TITLE')
+else:
+    print('[FAIL] TITLE')
 
+# Test Case 2
+if driver.current_url == 'https://www.google.com/':
+    print('[PASS] URL')
+else:
+    print('[FAIL] URL')
 
+# Search something
+search_tex_box = driver.find_element_by_name('q')
+search_tex_box.clear()
+search_tex_box.send_keys('Selenium')
+
+# Close Driver
+driver.close()
